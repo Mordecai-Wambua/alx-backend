7122b742723e919d9bd982f5c242f0bec15ccd04
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-20 21:02:05 +0300

    Task 0: Basic dictionary(corrrection)

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,7 +13,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Add an item in the cache."""
         if key is None or item is None:
-            pass
+            return
         self.cache_data.update({key: item})
 
     def get(self, key):
