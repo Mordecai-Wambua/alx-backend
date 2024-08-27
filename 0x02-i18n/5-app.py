455b5d71a97f8bb6147b427f8ceb4c199990d321
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-08-27 19:40:10 +0300

    Task 05

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Basic Babel setup."""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration of available languages in our app."""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determine best match with supported languages."""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """Returns a user dictionary or None."""
+    user_id = request.args.get('login_as')
+    if user_id:
+        user_id = int(user_id)
+        return users.get(user_id)
+    return None
+
+@app.before_request
+def before_request() -> None:
+    """Executed before all other functions"""
+    g.user = get_user()
+
+
+@app.route('/')
+def hello() -> str:
+    """Basic app for index.html template."""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
